78b5c9fac7084be2e8923377261c21ca8eef639d
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-12-11 22:18:44 -0300

    create entity role

--- src/roles/http/routes/roles.routes.ts
+++ src/roles/http/routes/roles.routes.ts
@@ -1,18 +1,19 @@
+import { Role } from "@roles/entities/Role";
 import { Request, Response, Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 
 const rolesRoutes = Router();
 
-const roles = [];
+const roles: Role[] = [];
 
 rolesRoutes.post("/", (request: Request, response: Response) => {
   const { name } = request.body;
 
-  const role = {
-    id: uuidv4(),
+  const role = new Role();
+
+  Object.assign(role, {
     name,
     created_at: new Date(),
-  };
+  });
 
   roles.push(role);
 
