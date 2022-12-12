ae883ea43d6c19ba746a2b5fefbef360a1de4578
Ryan Gualberto <ryanoliveiragualberto@gmail.com>
2022-12-11 22:02:15 -0300

    create app error middleware

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,9 +1,11 @@
+import { AppError } from "@shared/errors/AppError";
 import { Router } from "express";
 
 const routes = Router();
 
 routes.get("/", (request, response) => {
-  response.json({
+  throw new AppError("Acesso Negado", 403);
+  return response.json({
     message: "Olá dev",
   });
 });
